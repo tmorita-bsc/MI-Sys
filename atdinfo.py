3560bd12186a64a2d66e8e674ffea7233d4521ef
tmorita-bsc <tatsuya.morita@bosco-tech.com>
2018-01-04 17:06:43 +0900

    modify using query parameters

--- atdinfo.py
+++ atdinfo.py
@@ -12,34 +12,24 @@ weekdays = ["月", "火", "水", "木", "金", "土", "日"]
 class AtdData(Resource):
     TABLE_NAME = 'atd_table'
 
-    parser = reqparse.RequestParser()
-    #parser.add_argument('arrival_time',
-    #        type=str,
-    #        required=True,
-    #        help="This field cannot be left blank!"
-    #        )
-    #parser.add_argument('leave_time',
-    #        type=str,
-    #        required=True,
-    #        help="This field cannot be left blank!"
-    #        )
-
     #@jwt_required()
     def get(self, name):
-        atd_info = self.find_by_name(name)
+        # get query parameters
+        atd_info = self.find_by_name(name, request.args)
+
         if atd_info:
             return atd_info
         return {'message': 'User not Found'}, 404
 
     @classmethod
-    def find_by_name(cls, name):
+    def find_by_name(cls, name, params):
         connection = sqlite3.connect('atd_info.db')
         cursor = connection.cursor()
         
-        #query = "SELECT * FROM {table} WHERE name=?".format(table=cls.TABLE_NAME)
-        # most latest date in user
-        query = "SELECT * FROM {table} WHERE date=(SELECT MAX(date) FROM {table} WHERE name=?)".format(table=cls.TABLE_NAME)
-        result = cursor.execute(query, (name,))
+        search_date = params['year'] + "-" + params['month'] + "-" + params['day']
+
+        query = "SELECT * FROM {table} WHERE date=? AND name=?".format(table=cls.TABLE_NAME)
+        result = cursor.execute(query, (search_date, name,))
         row = result.fetchone()
         connection.close()
 
@@ -55,11 +45,8 @@ class AtdData(Resource):
                 }
 
     def post(self, name):
-        if self.find_by_name(name):
+        if self.find_by_name(name, request.args):
             return {'message': "An atd_info with name '{}' already exist.".format(name)}
-
-        #atd_info = request.get_json()
-        #atd_info = AtdData.parser.parse_args()
 
         insert_atdinfo = { \
                 "name": name, \
@@ -76,13 +63,13 @@ class AtdData(Resource):
 
         return insert_atdinfo
 
-    #def get_time(self):
-    #    return datetime.datetime.now()
+    def get_time(self):
+        return datetime.datetime.now()
 
     # dummy
-    def get_time(self):
+    #def get_time(self):
         # return datetime.date(2017, 1, 8)  # sunday
-        return datetime.date(2018, 1, 9)  # holiday monday
+        #return datetime.date(2018, 1, 12)  # holiday monday
     
     @classmethod
     def insert(cls, insert_atdinfo): # insert arrival_time
@@ -106,8 +93,11 @@ class AtdData(Resource):
         connection = sqlite3.connect('atd_info.db')
         cursor = connection.cursor()
         
+        params = request.args
+        search_date = params['year'] + "-" + params['month'] + "-" + params['day']
+
         query = "UPDATE {table} SET arrival_time=?, leave_time=? WHERE name=? AND date=?".format(table=self.TABLE_NAME)
-        cursor.execute( query, ("", "", name, self.get_time().strftime("%Y-%m-%d") ))
+        cursor.execute( query, ("", "", name, search_date ))
 
         connection.commit()
         connection.close()
@@ -116,8 +106,8 @@ class AtdData(Resource):
 
     #@jwt_required()
     def put(self, name): # update leave_time
-        #atd_info = AtdData.parser.parse_args() 
-        prev_atdinfo = self.find_by_name(name)
+        prev_atdinfo = self.find_by_name(name, request.args)
+        ##return prev_atdinfo['atd_info']['date']
 
         if prev_atdinfo is None or prev_atdinfo['atd_info']['date'] < self.get_time().strftime("%Y-%m-%d"):
             try:
@@ -131,6 +121,7 @@ class AtdData(Resource):
                 AtdData.insert(update_atdinfo)
             except:
                 return {"message" : "An error occurred inserting the atd_info."}
+
         elif prev_atdinfo['atd_info']['date'] == self.get_time().strftime("%Y-%m-%d"):
             try:
                 update_atdinfo = { \
@@ -140,6 +131,7 @@ class AtdData(Resource):
                 AtdData.update(update_atdinfo)
             except:
                 return {"message" : "An error occured updating the atd_info."}
+
         else:
             return {"message" : "ERROR"}
 
@@ -149,9 +141,12 @@ class AtdData(Resource):
     def update(cls, atd_info):
         connection = sqlite3.connect('atd_info.db')
         cursor = connection.cursor()
+
+        params = request.args
+        search_date = params['year'] + "-" + params['month'] + "-" + params['day']
         
-        query = "UPDATE {table} SET leave_time=? WHERE name=?".format(table=cls.TABLE_NAME)
-        cursor.execute( query, (atd_info['leave_time'], atd_info['name']))
+        query = "UPDATE {table} SET leave_time=? WHERE name=? AND date=?".format(table=cls.TABLE_NAME)
+        cursor.execute( query, (atd_info['leave_time'], atd_info['name'], search_date))
 
         connection.commit()
         connection.close()
@@ -164,13 +159,22 @@ class AtdDataList(Resource):
         connection = sqlite3.connect('atd_info.db')
         cursor = connection.cursor()
 
-        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
-        result = cursor.execute(query)
+        params = request.args
+        search_date = params['year'] + "-" + params['month'] + "-" + params['day']
+
+        query = "SELECT * FROM {table} WHERE date=?".format(table=self.TABLE_NAME)
+        result = cursor.execute(query, (search_date,))
 
         atd_infolist = []
         
         for row in result:
-            atd_infolist.append({'name': row[0], 'arrival_time': row[1], 'leave_time': row[2]})
+            atd_infolist.append({ \
+                    'name': row[0], \
+                    'date': row[1], \
+                    'weekday': row[2], \
+                    'arrival_time': row[3], \
+                    'leave_time': row[4] \
+                    })
 
         connection.close()
 
